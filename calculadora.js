98302159553db94844e6af899bf3da2fcd4e3054
LuizR3is <luiz.maia04@gmail.com>
2023-09-21 12:19:49 -0300

    add funcionalidades de raíz, potência e fatorial

--- calculadora.js
+++ calculadora.js
@@ -41,8 +41,44 @@ function calcular(){
 
 //Função pra manipular as demais operações
 function calcular_extra(op){
-    if(op == '√'){
-        
-    }
+    resultado = document.getElementById('resultado').innerHTML;
 
+    var valor;
+    var r;
+
+    //Caso tenham operações em sequência, faz um recorte da operação especial para calcular individualmente e depois substitui pelo resultado na calculadora
+    if(isNaN(resultado)){
+        for(var i=resultado.length-1; i>=0; i--){
+            if(isNaN(resultado[i])){
+                valor = resultado.slice(i+1, resultado.length);
+                r = resultado.slice(0, i+1);
+                break;
+            }
+        }
+    } else{
+        r = '';
+        valor = resultado;
+    }
+    
+    
+    if(op == '√'){
+        r = r + Math.sqrt(valor);
+    } else if(op == 'x²'){
+        r = r + Math.pow(valor, 2);
+    } else if(op == 'n!'){
+        r = r + fatorial(valor);
+    }
+    
+    document.getElementById('resultado').innerHTML = r;
+
+}
+
+function fatorial(n){
+    if(n < 0){
+        return -1;
+    } else if(n == 0){
+        return 1;
+    } else{
+        return (n*fatorial(n-1));
+    }
 }
